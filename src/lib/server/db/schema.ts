3b6feb238788291a6dcbd286e27187656eb69fd0
Codeunic <rafael.gonzalez.1737@gmail.com>
2025-03-01 19:20:39 +0100

    Initial commit

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,51 +1,50 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
-  id: text('id').primaryKey(),
-  age: integer('age'),
-  username: text('username').notNull().unique(),
-  passwordHash: text('password_hash').notNull()
+	id: text('id').primaryKey(),
+	age: integer('age'),
+	username: text('username').notNull().unique(),
+	passwordHash: text('password_hash').notNull()
 });
 
 export const session = sqliteTable('session', {
-  id: text('id').primaryKey(),
-  userId: text('user_id')
-    .notNull()
-    .references(() => user.id),
-  expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
 export const project = sqliteTable('project', {
-  id: text('id').primaryKey(),
-  name: text('name').notNull().unique(),
-  description: text('description').notNull(),
-  ownerId: text('owner_id')
-    .notNull()
-    .references(() => user.id),
-  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull()
+	id: text('id').primaryKey(),
+	name: text('name').notNull().unique(),
+	description: text('description').notNull(),
+	ownerId: text('owner_id')
+		.notNull()
+		.references(() => user.id),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull()
 });
 
 export const database = sqliteTable('database', {
-  id: text('id').primaryKey(),
-  name: text('name').notNull().unique(),
-  description: text('description').notNull(),
-  type: text('type').notNull(),
-  host: text('host').notNull(),
-  port: integer('port').notNull(),
-  username: text('username').notNull(),
-  password: text('password').notNull(),
-  default_database: text('default_database').notNull(),
-  projectId: text('project_id')
-    .notNull()
-    .references(() => project.id),
-  ownerId: text('owner_id')
-    .notNull()
-    .references(() => user.id),
-  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull()
+	id: text('id').primaryKey(),
+	name: text('name').notNull().unique(),
+	description: text('description').notNull(),
+	type: text('type').notNull(),
+	host: text('host').notNull(),
+	port: integer('port').notNull(),
+	username: text('username').notNull(),
+	password: text('password').notNull(),
+	default_database: text('default_database').notNull(),
+	projectId: text('project_id')
+		.notNull()
+		.references(() => project.id),
+	ownerId: text('owner_id')
+		.notNull()
+		.references(() => user.id),
+	createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
+	updatedAt: integer('updated_at', { mode: 'timestamp' }).notNull()
 });
-
 
 export type Database = typeof database.$inferSelect;
 export type Project = typeof project.$inferSelect;
--- src/routes/demo/lucia/login/+page.server.ts
+++ src/routes/demo/lucia/login/+page.server.ts
@@ -8,63 +8,63 @@ import * as table from '$lib/server/db/schema';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
-  if (event.locals.user) {
-    return redirect(302, '/demo/lucia');
-  }
-  return {};
+	if (event.locals.user) {
+		return redirect(302, '/demo/lucia');
+	}
+	return {};
 };
 
 export const actions: Actions = {
-  register: async (event) => {
-    const formData = await event.request.formData();
-    const username = formData.get('username');
-    const password = formData.get('password');
+	register: async (event) => {
+		const formData = await event.request.formData();
+		const username = formData.get('username');
+		const password = formData.get('password');
 
-    if (!validateUsername(username)) {
-      return fail(400, { message: 'Invalid username' });
-    }
-    if (!validatePassword(password)) {
-      return fail(400, { message: 'Invalid password' });
-    }
+		if (!validateUsername(username)) {
+			return fail(400, { message: 'Invalid username' });
+		}
+		if (!validatePassword(password)) {
+			return fail(400, { message: 'Invalid password' });
+		}
 
-    const userId = generateUserId();
-    const passwordHash = await hash(password, {
-      // recommended minimum parameters
-      memoryCost: 19456,
-      timeCost: 2,
-      outputLen: 32,
-      parallelism: 1
-    });
+		const userId = generateUserId();
+		const passwordHash = await hash(password, {
+			// recommended minimum parameters
+			memoryCost: 19456,
+			timeCost: 2,
+			outputLen: 32,
+			parallelism: 1
+		});
 
-    try {
-      await db.insert(table.user).values({ id: userId, username, passwordHash });
+		try {
+			await db.insert(table.user).values({ id: userId, username, passwordHash });
 
-      const sessionToken = auth.generateSessionToken();
-      const session = await auth.createSession(sessionToken, userId);
-      auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
-    } catch (e) {
-      return fail(500, { message: 'An error has occurred' });
-    }
-    return redirect(302, '/demo/lucia');
-  }
+			const sessionToken = auth.generateSessionToken();
+			const session = await auth.createSession(sessionToken, userId);
+			auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+		} catch (e) {
+			return fail(500, { message: 'An error has occurred' });
+		}
+		return redirect(302, '/demo/lucia');
+	}
 };
 
 function generateUserId() {
-  // ID with 120 bits of entropy, or about the same as UUID v4.
-  const bytes = crypto.getRandomValues(new Uint8Array(15));
-  const id = encodeBase32LowerCase(bytes);
-  return id;
+	// ID with 120 bits of entropy, or about the same as UUID v4.
+	const bytes = crypto.getRandomValues(new Uint8Array(15));
+	const id = encodeBase32LowerCase(bytes);
+	return id;
 }
 
 function validateUsername(username: unknown): username is string {
-  return (
-    typeof username === 'string' &&
-    username.length >= 3 &&
-    username.length <= 31 &&
-    /^[a-z0-9_-]+$/.test(username)
-  );
+	return (
+		typeof username === 'string' &&
+		username.length >= 3 &&
+		username.length <= 31 &&
+		/^[a-z0-9_-]+$/.test(username)
+	);
 }
 
 function validatePassword(password: unknown): password is string {
-  return typeof password === 'string' && password.length >= 6 && password.length <= 255;
+	return typeof password === 'string' && password.length >= 6 && password.length <= 255;
 }
